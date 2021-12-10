71362f94010411d6416bc1092a677a2abaa00808
HBLCH <91760048+HBLCH@users.noreply.github.com>
2021-12-10 12:24:14 -0500

    Update InvalidLoginResponse.java

--- src/main/java/com/creighbattle/exceptions/InvalidLoginResponse.java
+++ src/main/java/com/creighbattle/exceptions/InvalidLoginResponse.java
@@ -1,17 +1,41 @@
 package com.creighbattle.exceptions;
 
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
-@Getter
-@Setter
-@NoArgsConstructor
 public class InvalidLoginResponse {
-    private String username;
-    private String password;
+   
 
+
+private String username;
+private String password;
+
+
+
+public InvalidLoginResponse() {
+this.username = "Invalid Username";
+this.password = "Invalid Password";
+}
+
+
+
+public String getUsername() {
+return username;
+}
+
+
+
+public void setUsername(String username) {
+this.username = username;
+}
+
+
+
+public String getPassword() {
+return password;
+}
+
+
+
+public void setPassword(String password) {
+this.password = password;
+}
 }
